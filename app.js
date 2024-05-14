70cad8de65451db9345679470ed1ec474ff3c36a
InnocentiaHlongwane <innocentiah42@gmail.com>
2024-05-14 23:44:08 +0200

    added an image

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 function shopOnline() {
+
     let name = prompt ("What is your name?");
-    alert (
-        "Exactly what you looking for!" + " " + name + " " + "iterms that perfectly work with your wardrobe");
+    alert ("Exactly what you looking for!" + " " + name + " " + "iterms that perfectly work with your wardrobe");
 
     let email = prompt ("Sign up to recieve our catalogue, enter your email address.");
-    alert(
-        "catalogue will be sent to" + " " + email + " " + ", Happy shopping🛍️"
-    );
+    alert( "catalogue will be sent to" + " " + email + " " + ", Happy shopping🛍️" );
 }
 
     let shopButton = document.querySelector("button");
-    shopButton.addEventListener("click, shopOnline");
+    shopButton.addEventListener("click", shopOnline);
